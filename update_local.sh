d8ba97a88f202478fd32b8a9016ab7da3afb5316
bangvo <bavo.imp@gmail.com>
2022-06-29 15:14:55 +0700

    minor

--- update_local.sh
+++ update_local.sh
@@ -1,14 +1,10 @@
 # nvim
 mkdir -p ~/.config/nvim/
 cp ./.config/nvim/init.vim ~/.config/nvim/init.vim 
-cp ./.config/nvim/coc.vim ~/.config/nvim/coc.vim 
 
 # coc
 cp ./.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json 
-
-# yapf
-mkdir -p ~/.config/yapf/
-cp ./.config/yapf/style  ~/.config/yapf/style
+cp ./.config/nvim/coc.vim ~/.config/nvim/coc.vim 
 
 # tmux
 cp ./.tmux.conf ~/.tmux.conf
--- update_repo.sh
+++ update_repo.sh
@@ -1,12 +1,9 @@
 # nvim
 cp ~/.config/nvim/init.vim ./.config/nvim/init.vim 
-cp ~/.config/nvim/coc.vim ./.config/nvim/coc.vim 
 
 # coc
 cp ~/.config/nvim/coc-settings.json ./.config/nvim/coc-settings.json
-
-# yapf
-cp ~/.config/yapf/style ./.config/yapf/style 
+cp ~/.config/nvim/coc.vim ./.config/nvim/coc.vim 
 
 # tmux
 cp ~/.tmux.conf ./.tmux.conf

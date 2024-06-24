81424dceef18a6f53e8e12079b2ca197ee6124e5
Vo Quoc Bang <bavo.imp@gmail.com>
2024-06-24 22:08:32 +0700

    chore: show additional information in lualine

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -11,12 +11,22 @@ return {
                     theme = 'gruvbox'
                 },
                 sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { 'filename', 'filesize', function()
+                        local current_buf = 'current_buf: ' .. vim.api.nvim_get_current_buf()
+                        return current_buf
+                    end },
+                    lualine_x = { 'encoding' },
+
                     lualine_y = {
-                        function() -- Total line in file
-                            return vim.fn.line('$')
+                        function() -- Get total line in file
+                            local total_line_info = 'total_line: ' .. vim.fn.line('$')
+                            return total_line_info
                         end,
                         'progress' -- Progress bar
-                    }
+                    },
+                    lualine_z = { 'location' }
                 }
 
             })

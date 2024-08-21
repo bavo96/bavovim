65d5d743dc66741eb8293e7e726f62e2771cb3cf
Bang Vo <bavo.imp@gmail.com>
2024-08-21 13:44:37 +0700

    feat: add install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+echo "Installing necessary package for neovim..."
 bash ./scripts/update_local.sh
-pip install -r requirements.txt
+if [[ "$(python3 -V)" =~ "Python 3" ]]; then
+    echo "Python 3 is installed. Installing necessary packages..."
+    pip install -r requirements.txt
+fi
 
-# if [ "$(uname)" == "Darwin" ]; then
-#     brew install 
-# elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-#     echo "Windows is not supported"
-# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-#     echo "Windows is not supported"
-# fi 
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "MacOS detected. Installing necessary packages..."
+    brew install ripgrep
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+    echo "Linux detected. Installing necessary packages..."
+    apt-get install ripgrep
+elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]]; then
+    echo "Windows is not supported."
+elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
+    echo "Windows is not supported."
+fi 

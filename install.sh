aaef09fe253138faab41899d79ef481e1845fdbb
Bang Vo <bavo.imp@gmail.com>
2024-09-07 14:34:57 +0700

    feat: create installer

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "Installing necessary package for neovim..."
-bash ./scripts/update_local.sh
+
+### Install necessary packages for python
 if [[ "$(python3 -V)" =~ "Python 3" ]]; then
-    echo "Python 3 is installed. Installing necessary packages..."
+    echo "Python $(python3 -V) is installed. Installing necessary packages..."
     pip install -r requirements.txt
+else
+    echo "Python is not installed. Please install python3 and try again."
 fi
 
+### Install necessary packages for OS
 if [[ "$(uname)" == "Darwin" ]]; then
     echo "MacOS detected. Installing necessary packages..."
     brew install ripgrep
@@ -18,3 +22,6 @@ elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]]; then
 elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
     echo "Windows is not supported."
 fi 
+
+### Copy neovim config files
+bash ./scripts/update_local.sh

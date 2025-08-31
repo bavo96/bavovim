1913f917e3f4d028357672923d2dd3e17263bc09
Bang Vo <bavo.imp@gmail.com>
2025-08-31 15:50:03 +0700

    chore: update code

--- .config/nvim/lua/plugins/tools/avante-nvim.lua
+++ .config/nvim/lua/plugins/tools/avante-nvim.lua
@@ -1,26 +1,42 @@
 return {
     "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
     opts = {
         -- add any opts here
+        -- this file can contain specific instructions for your project
+        instructions_file = "avante.md",
         -- for example
-        provider = "openai",
-        openai = {
-            endpoint = "https://api.openai.com/v1",
-            model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
-            timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
-            temperature = 0,
-            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        provider = "claude",
+        providers = {
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-sonnet-4-20250514",
+                timeout = 30000, -- Timeout in milliseconds
+                extra_request_body = {
+                    temperature = 0.75,
+                    max_tokens = 20480,
+                },
+            },
+            moonshot = {
+                endpoint = "https://api.moonshot.ai/v1",
+                model = "kimi-k2-0711-preview",
+                timeout = 30000, -- Timeout in milliseconds
+                extra_request_body = {
+                    temperature = 0.75,
+                    max_tokens = 32768,
+                },
+            },
         },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
@@ -28,6 +44,8 @@ return {
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
         "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
         "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+        "stevearc/dressing.nvim",        -- for input provider dressing
+        "folke/snacks.nvim",             -- for input provider snacks
         "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
         "zbirenbaum/copilot.lua",        -- for providers='copilot'
         {

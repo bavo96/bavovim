66a0c6942ff3fbb4d37b8d432cf22e61ddff7db9
Bang Vo <bavo.imp@gmail.com>
2025-10-15 22:23:24 +0700

    chore: update

--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -7,15 +7,14 @@ return {
 
         -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
         for _, client in ipairs(lsputils.clients) do
-            require("lspconfig")[client].setup(
-                {
-                    settings = lsputils.settings(client),
-                    init_options = lsputils.init_options(client),
-                    filetypes = lsputils.filetypes(client),
-                    capabilities = lsputils.capabilities(),
-                    on_attach = lsputils.on_attach,
-                }
-            )
+            vim.lsp.config[client] = {
+                settings = lsputils.settings(client),
+                init_options = lsputils.init_options(client),
+                filetypes = lsputils.filetypes(client),
+                capabilities = lsputils.capabilities(),
+                on_attach = lsputils.on_attach,
+            }
         end;
+        vim.lsp.enable(lsputils.clients)
     end
 }
